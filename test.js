b0db975ca580a25a519b8faccfaff73a384b525f
Steve Kane <kanesteven@gmail.com>
2016-07-13 16:33:49 -0500

    updated package to graph-clone and correct dependencies for dev

---
+++ test.js
@@ -0,0 +1,77 @@
+var test = require('tape')
+var sbs = require('log-side-by-side')
+var UUID = require('node-uuid')
+var gMap = require('./index')
+
+class Doc {
+  constructor(nodes, media) {
+    this.Type = 'Edition'
+    this.objectId = UUID()
+    this.nodes = nodes
+    this.media = media
+    this.selected = null
+  }
+}
+
+class Node {
+  constructor(name) { 
+    this.Type = 'Node'
+    this.objectId = UUID()
+    this.name = name
+    this.stage = new Asset
+  }
+}
+
+class Media {
+  constructor() {
+    this.Type = 'Media'
+    this.objectId = UUID() 
+  }
+}
+
+class Asset {
+  constructor() {
+    this.Type = 'Asset'
+    this.objectId = UUID()
+    this.children = []
+    this.media = null
+    this.parent = null
+  }
+
+  setParent(parent) {
+    if ( this.parent ) this.parent.children.splice(this.parent.children.indexOf(this), 1)
+    this.parent = parent 
+    if ( parent.children.indexOf(this) === -1 ) parent.children.push(this)
+  }
+}
+
+test('copy is truly deepcloned', t => {
+  var e = new Doc(
+    [ new Node('sequence 1') ],
+    [ new Media, new Media ])
+
+  var a1 = new Asset
+  var a2 = new Asset
+
+  //e.selected = e.nodes[0]
+  a1.setParent(e.nodes[0].stage)
+  a1.media = e.media[0]
+  a2.setParent(a1)
+
+  var copy = gMap(e, e => e instanceof Media, (t, k, v) => k === 'objectId' ? UUID() : v)
+
+  console.log(sbs(10, e, copy))
+  t.true(e !== copy, 'not equal')
+  t.true(e.nodes !== copy.nodes)
+  t.true(e.media[0] === copy.media[0])
+  t.true(e.nodes[0] !== copy.nodes[0])
+  t.true(e.nodes[0].stage !== copy.nodes[0].stage)
+  t.true(e.nodes[0].stage.children[0] !== copy.nodes[0].stage.children[0])
+  t.true(e.nodes[0].stage.children[0] !== copy.nodes[0].stage.children[0])
+  t.true(e.nodes[0].stage.children[0].children[0] !== copy.nodes[0].stage.children[0].children[0])
+  t.true(e.nodes[0].stage.children[0].parent !== copy.nodes[0].stage.children[0].parent)
+  t.true(e.nodes[0].stage.children[0].children[0].parent !== copy.nodes[0].stage.children[0].children[0].parent)
+  t.true(e.nodes[0].stage.objectId !== copy.nodes[0].stage.objectId)
+  t.true(e.nodes[0].media === copy.nodes[0].media)
+  t.end()
+})
